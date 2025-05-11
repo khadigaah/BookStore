bc3801f152b6c4a1cd19813a1269fe8d1289fc7d
Khadiga Hany <khadiga.dev@gmail.com>
2025-05-11 19:59:55 +0300

    refactor init state , naviga to home and remove ic_launcher.png

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,10 @@
+import 'package:bookly/Features/home/presentation/views/home_view.dart';
 import 'package:bookly/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -20,18 +23,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
 
     super.initState();
+    initSlidingAnimation();
 
-    animationController = AnimationController(
-        vsync: this,
-        duration: const Duration(seconds: 1));
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
-            .animate(animationController);
-
-      slidingAnimation.addListener(() {
-        setState(() {});
-      });
-        animationController.forward();
+      navigateToHome();
   }
 
   @override
@@ -52,5 +46,24 @@ class _SplashViewBodyState extends State<SplashViewBody>
       SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+      slidingAnimation.addListener(() {
+        setState(() {});
+      });
+        animationController.forward();
+  }
+    void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+    Get.to(() => const HomeView(), transition: Transition.fade, duration: kTranstionDuration);
+      });
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff000000);
 const kSecondaryColor = Color(0xffF7F7F7);
-const kTranstionDuration = Duration(milliseconds: 250);
+const kTranstionDuration = Duration(milliseconds: 350);
 const kGtSectraFine = 'GT Sectra Fine';
