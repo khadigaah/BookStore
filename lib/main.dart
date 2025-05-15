b3007d3ef2798463d7cd02aa2367f21d6de4e7e5
Khadiga Hany <khadiga.dev@gmail.com>
2025-05-15 16:02:31 +0300

    add font family to our app and remove some images.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
 import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       home: const SplashView(),
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: TextTheme(),
+        textTheme: GoogleFonts.montserratTextTheme(),
       ),
     );
   }
