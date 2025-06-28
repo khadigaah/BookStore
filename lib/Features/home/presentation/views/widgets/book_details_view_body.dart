a964031a6554ba2da1434595b328039c9b71c128
Khadiga Hany <khadiga.dev@gmail.com>
2025-06-28 15:12:42 +0300

    add title, subtitle and icon to book details view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -15,11 +17,41 @@ class BookDetailsViewBody extends StatelessWidget {
           const CustomBookDetailsAppBar(),
           Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: width * .17,
+              horizontal: width * .18,
               vertical: 20,
             ),
             child: const CustomBookImage(),
           ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.titleSmall30,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.titleSmall16.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+          const Icon(FontAwesomeIcons.solidStar, color: Colors.amber, size: 12),
+        const SizedBox(width: 5),
+        const Text('4.8', style: Styles.titleSmall16),
+        const SizedBox(width: 5),
+        const Text('(1234)', style: Styles.titleSmall14),
+          ],)
         ],
       ),
     );
