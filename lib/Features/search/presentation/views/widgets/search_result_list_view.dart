f88cc74c27a2a6eb2b73b6d85318915d7392ae93
Khadiga Hany <khadiga.dev@gmail.com>
2025-06-29 17:35:24 +0300

    search result list view body ui done

--- lib/Features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/Features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,19 +1,21 @@
+
 import 'package:bookly/Features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: List.generate(
-        10,
-        (index) => const Padding(
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
           padding: EdgeInsets.symmetric(vertical: 10),
           child: BookListViewItem(),
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,6 @@
-
 import 'package:bookly/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly/Features/search/presentation/views/widgets/search_result_list_view.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -7,13 +8,16 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25),
-          child: CustomSearchTextField(),
-        ),
-      ],
+    return Padding(padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const[
+      CustomSearchTextField(),
+      SizedBox(height: 16,),
+      Text('Search Result', style: Styles.titleMedium,),
+      SizedBox(height: 16,),
+      Expanded(child: SearchResultListView()),
+    ],),
     );
   }
 }
