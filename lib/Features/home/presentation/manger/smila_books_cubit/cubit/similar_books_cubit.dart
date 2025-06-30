9c9118df9f42a7ce6de5a141aec8a617d777ed86
Khadiga Hany <khadiga.dev@gmail.com>
2025-07-01 02:21:43 +0300

    similar books state done

--- lib/Features/home/presentation/manger/smila_books_cubit/cubit/similar_books_cubit.dart
+++ lib/Features/home/presentation/manger/smila_books_cubit/cubit/similar_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
 
 part 'similar_books_state.dart';
--- lib/Features/home/presentation/manger/smila_books_cubit/cubit/similar_books_state.dart
+++ lib/Features/home/presentation/manger/smila_books_cubit/cubit/similar_books_state.dart
@@ -8,3 +8,13 @@ sealed class SimilarBooksState extends Equatable {
 }
 
 final class SimilarBooksInitial extends SimilarBooksState {}
+final class SimilarBooksLoading extends SimilarBooksState {}
+
+final class SimilarBooksSuccess extends SimilarBooksState {
+  final List<BookModel> books;
+  const SimilarBooksSuccess(this.books);
+}
+final class SimilarBooksFailure extends SimilarBooksState {
+  final String errMessage;
+  const SimilarBooksFailure(this.errMessage);
+}
